a1fee08dd1cb71a3bc0680a2437f38c58362ea03
Jeeran <jeeran@gmail.com>
2016-09-23 11:11:47 +0100

    passport-Jwat

--- node-express-gen/app.js
+++ node-express-gen/app.js
@@ -4,8 +4,23 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var session = require('express-session');
-var FileStore = require('session-file-store')(session);
+//var session = require('express-session');
+//var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+var config = require('./config');
+
+// // Mongoose connection
+var mongoose = require('mongoose');
+mongoose.connect(config.url);
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    // we're connected!
+    console.log("Connected correctly to server");
+});
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -26,81 +41,67 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//app.use(cookieParser());
-app.use(cookieParser('12345-67890-09876-54321')); // secret key
+app.use(cookieParser());
+//app.use(cookieParser('12345-67890-09876-54321')); // secret key
 
-// Session
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: true,
-  resave: true,
-  store: new FileStore()
-}));
+// // Session
+// app.use(session({
+//   name: 'session-id',
+//   secret: '12345-67890-09876-54321',
+//   saveUninitialized: true,
+//   resave: true,
+//   store: new FileStore()
+// }));
 
-// Authentication
-var auth = require('./routes/auth');
-app.use(auth);
-// Authentication
-//app.use(auth);
+// // Authentication
+// var auth = require('./routes/auth');
+// app.use(auth);
+
+// passport config
+var User = require('./models/user');
+app.use(passport.initialize());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', routes);
-// app.use('/users', users);
+app.use('/', routes);
+app.use('/users', users);
 
-// app.use('/dishes', dishes);
-// app.use('/promotions', promotions);
-// app.use('/leaders', leaders);
+app.use('/dishes', dishes);
+app.use('/promotions', promotions);
+app.use('/leaders', leaders);
 
-// catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-
-// // error handlers
-
-// // development error handler
-// // will print stacktrace
-// if (app.get('env') === 'development') {
-//     app.use(function(err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//         message: err.message,
-//         error: err
-//     });
-//   });
-// }
-
-// // production error handler
-// // no stacktraces leaked to user
-// app.use(function(err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//     message: err.message,
-//     error: {}
-//   });
-// });
-
-app.use(function(err,req,res,next) {
-    res.writeHead(err.status || 500, {
-    'WWW-Authenticate': 'Basic',
-    'Content-Type': 'text/plain'
-    });
-    res.end(err.message);
+//catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-// // Mongoose connection
-// var mongoose = require('mongoose');
+// error handlers
 
-// var url = 'mongodb://localhost:27017/conFusion';
-// mongoose.connect(url);
-// var db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function () {
-//     // we're connected!
-//     console.log("Connected correctly to server");
-// });
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 
 module.exports = app;
--- node-express-gen/routes/dishes.js
+++ node-express-gen/routes/dishes.js
@@ -1,24 +1,25 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var Dishes = require('../models/dishes')
+var Verify = require('./verify')
 
 var router = express.Router();
 router.use(bodyParser.json());
 
 router.route('/')
-.all(function(req,res,next) { 
+.all(Verify.verifyOrdinaryUser, function(req,res,next) { 
   // res.writeHead(200, { 'Content-Type': 'text/plain' });
     next();
 })
 
-.get(function(req,res,next) { 
+.get(Verify.verifyOrdinaryUser, function(req,res,next) { 
     Dishes.find({}, function (err, dish) { 
         if (err) throw err;
         res.json(dish);
     });
 })
 
-.post(function(req, res, next) {
+.post(Verify.verifyOrdinaryUser, function(req, res, next) {
     Dishes.create(req.body, function (err, dish) {
         if (err) throw err;
         console.log('Dish created!');
@@ -31,7 +32,7 @@ router.route('/')
     });
 })
 
-.delete(function(req, res, next){
+.delete(Verify.verifyOrdinaryUser, function(req, res, next){
     Dishes.remove({}, function (err, resp) {
         if (err) throw err;
         res.json(resp);
@@ -39,29 +40,29 @@ router.route('/')
 });
 
 router.route('/:dish')
-.all(function(req,res,next) {
+.all(Verify.verifyOrdinaryUser, function(req,res,next) {
     next();
 })
 
-.get(function(req,res,next){
+.get(Verify.verifyOrdinaryUser, function(req,res,next){
     Dishes.findById(req.params.dish, function (err, dish) {
         if (err) throw err;
         res.json(dish);
     });
 })
 
-.put(function(req, res, next){
+.put(Verify.verifyOrdinaryUser, function(req, res, next){
     Dishes.findByIdAndUpdate(req.params.dishId, {
         $set: req.body
         }, {
-        new: true
+            new: true
         }, function (err, dish) {
-        if (err) throw err;
-        res.json(dish);
+            if (err) throw err;
+            res.json(dish);
         });
 })
 
-.delete(function(req, res, next){
+.delete(Verify.verifyOrdinaryUser, function(req, res, next){
     Dishes.findByIdAndRemove(req.params.dishId, function (err, resp) {   
         if (err) throw err;
         res.json(resp);
@@ -70,14 +71,14 @@ router.route('/:dish')
 
 // Comments
 router.route('/:dish/comments')
-.get(function (req, res, next) {
+.get(Verify.verifyOrdinaryUser, function (req, res, next) {
     Dishes.findById(req.params.dishId, function (err, dish) {
         if (err) throw err;
         res.json(dish.comments);
     });
 })
 
-.post(function (req, res, next) {
+.post(Verify.verifyOrdinaryUser, function (req, res, next) {
     Dishes.findById(req.params.dishId, function (err, dish) {
         if (err) throw err;
         dish.comments.push(req.body);
@@ -89,7 +90,7 @@ router.route('/:dish/comments')
     });
 })
 
-.delete(function (req, res, next) {
+.delete(Verify.verifyOrdinaryUser, function (req, res, next) {
     Dishes.findById(req.params.dishId, function (err, dish) {
         if (err) throw err;
         for (var i = (dish.comments.length - 1); i >= 0; i--) {
@@ -107,14 +108,14 @@ router.route('/:dish/comments')
 });
 
 router .route('/:dish/comments/:comment')
-.get(function (req, res, next) {
+.get(Verify.verifyOrdinaryUser, function (req, res, next) {
     Dishes.findById(req.params.dishId, function (err, dish) {
         if (err) throw err;
         res.json(dish.comments.id(req.params.comment));
     });
 })
 
-.put(function (req, res, next) {
+.put(Verify.verifyOrdinaryUser, function (req, res, next) {
     // We delete the existing commment and insert the updated
     // comment as a new comment
     Dishes.findById(req.params.dishId, function (err, dish) {
@@ -129,7 +130,7 @@ router .route('/:dish/comments/:comment')
     });
 })
 
-.delete(function (req, res, next) {
+.delete(Verify.verifyOrdinaryUser, function (req, res, next) {
     Dishes.findById(req.params.dishId, function (err, dish) {
         dish.comments.id(req.params.comment).remove();
         dish.save(function (err, resp) {
